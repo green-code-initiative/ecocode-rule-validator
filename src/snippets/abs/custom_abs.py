d05890f4fa6da6ba201dfbf48773211fc8b07804
m-bonati <m.bonati@outlook.fr>
2024-06-03 17:00:07 +0200

    reorganize folders for snippets and configs

--- src/snippets/abs/custom_abs.py
+++ src/snippets/abs/custom_abs.py
@@ -1,15 +1,18 @@
 from argparse import ArgumentParser
 
+
 def _abs(x):
     if x > 0:
         return x
     else:
         return -x
 
+
 def main(x: int):
     n = 1000
     for _ in range(n):
         _abs(x)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
